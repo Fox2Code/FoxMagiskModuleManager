5324f5ac2e816a7ffa8fb40dc676b0b554b424c0
Fox2Code <fox2code@gmail.com>
2022-03-03 15:21:52 +0100

    0.4.0 Release Candidate 1

--- app/src/main/java/com/fox2code/mmm/compat/CompatActivity.java
+++ app/src/main/java/com/fox2code/mmm/compat/CompatActivity.java
@@ -19,7 +19,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewConfiguration;
-import android.view.WindowManager;
 
 import androidx.annotation.AttrRes;
 import androidx.annotation.CallSuper;
