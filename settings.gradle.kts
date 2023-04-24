9bcc1f6303954e1569148cd2ea189a12b1af468d
Androidacy Service Account <opensource@androidacy.com>
2023-04-23 22:01:40 -0400

    Add gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,17 @@
 @file:Suppress("UnstableApiUsage")
+plugins {
+    id("com.gradle.enterprise") version("3.9")
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
