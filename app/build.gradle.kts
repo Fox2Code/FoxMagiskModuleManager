b14ebd9d628e21f140d760f7fa6f3fb1a745fcea
Siqi Zhao <77391690+Z-Siqi@users.noreply.github.com>
2023-05-03 15:54:59 +0800

    Fix Simplified Chinese option not work
    Fix  incorrect language code.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
             useSupportLibrary = true
         }
         multiDexEnabled = true
-        resourceConfigurations.addAll(listOf("ar", "bs", "cs", "de", "es-rMX", "fr", "hu", "id", "ja", "nl", "pl", "pt", "pt-rBR", "ro", "ru", "tr", "uk", "zh", "zh-rTW", "en"))
+        resourceConfigurations.addAll(listOf("ar", "bs", "cs", "de", "es-rMX", "fr", "hu", "id", "ja", "nl", "pl", "pt", "pt-rBR", "ro", "ru", "tr", "uk", "zh-rCN", "zh-rTW", "en"))
     }
 
     splits {
